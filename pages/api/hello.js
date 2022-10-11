bb5e1bc87588b4636800bb8a808aa4feda51ab29
olandesino <olandesino84@gmail.com>
2022-10-11 11:44:14 +0200

    Update hello.js
    aaa

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -1,8 +1,6 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 
 export default function handler(req, res) {
-   console.log('request ', JSON.stringify(req.body))
-   JSON.stringify(req.body)
    res.status(200).json(req.body)
 //    res.status(200).json({ name: 'what Doe' })
 }
