e06914330f194a827c85ab5233f6e965b3cfe4fc
olandesino <olandesino84@gmail.com>
2022-10-11 12:43:01 +0200

    Update hello.js
    lll

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -1,6 +1,6 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 
-export default function ha(ndler(req, res) {
+export default function handler(req, res) {
    res.status(200).json(JSON.stringify(req.body))
 //    res.status(200).json({ name: 'what Doe' })
 }
